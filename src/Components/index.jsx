9605c2fa668f1d461601a816b7cb2e051c864832
Sarthak Kesarwani <srthkksrwnicool@gmail.com>
2022-08-16 22:04:32 +0530

    fwqeguy45i6

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -26,20 +26,17 @@ function Index() {
       </div>
       */}
       <section className="hero-card-web bg-gradient12 shape-bg3">
-        <div className="hero-main-rp container-fluid">
-          <div className="row">
-            <div className="col-lg-7">
+        <div className="hero-main-rp container-fluid p-5">
+          <div className="row d-flex justify-content-center">
+            <div className="col-lg-12">
               <div className="hero-heading-sec">
-                <h2 className="wow fadeIn" data-wow-delay="0.3s">
+                <h2 className="wow fadeIn text-center" data-wow-delay="0.3s">
                   <span>
                     Unlimited Graphics & Video Designs Flat Monthly Fees.
                     SUBSCRIBE NOW
                   </span>
                 </h2>
-                <p className="wow fadeIn" data-wow-delay="0.6s">
-                  One Graphic & Video Free for our New Clients
-                </p>
-
+                <div className="d-flex justify-content-center">
                 <a style={{marginRight:'20px'}}
                   href="case-study.html"
                   className="niwax-btn2 wow fadeIn"
@@ -55,177 +52,11 @@ function Index() {
                 >
                   Schedule demo <i className="fas fa-chevron-right fa-ani"></i>
                 </a>
-
-                <a
-                  href="case-study.html"
-                  className="niwax-btn2 wow fadeIn"
-                  data-wow-delay="0.8s"
-                >
-                  Video <i className="fas fa-chevron-right fa-ani"></i>
-                </a>
-
-                <div
-                  className="awards-block-tt  wow fadeIn"
-                  data-wow-delay="1s"
-                >
-                  <img
-                    src="./assets/images/hero/awards-logo.png"
-                    alt="awards-logo"
-                    className="img-fluid"
-                  />
                 </div>
+
               </div>
             </div>
-            <div className="col-lg-5">
-              <div
-                className="hero-content-sec wow fadeIn"
-                data-wow-delay="0.8s"
-              >
-                <div className="video-intro-pp">
-                  <a
-                    className="video-link play-video"
-                    href="https://www.youtube.com/watch?v=SZEflIVnhH8?autoplay=1&amp;rel=0"
-                  >
-                    <span className="triangle-play"></span>
-                  </a>
-                </div>
-                <div className="title-hero-oth">
-                  <p>
-                    We design digital solutions{" "}
-                    <span>for brands and companies</span>
-                  </p>
-                </div>
-              </div>
-              <div className="hero-right-scmm">
-                <div
-                  className="hero-service-cards wow fadeInRight"
-                  data-wow-duration="2s"
-                >
-                  <div className="owl-carousel service-card-prb">
-                    <div
-                      className="service-slide card-bg-a"
-                      data-tilt
-                      data-tilt-max="10"
-                      data-tilt-speed="1000"
-                    >
-                      <a href="#">
-                        <div className="service-card-hh">
-                          <div className="image-sr-mm">
-                            <img
-                              alt="custom-sport"
-                              src="./assets/images/service/vr.png"
-                            />
-                          </div>
-                          <div className="title-serv-c">
-                            <span>VR</span> Solution
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                    <div
-                      className="service-slide card-bg-b"
-                      data-tilt
-                      data-tilt-max="10"
-                      data-tilt-speed="1000"
-                    >
-                      <a href="#">
-                        <div className="service-card-hh">
-                          <div className="image-sr-mm">
-                            <img
-                              alt="custom-sport"
-                              src="./assets/images/service/app-develop.png"
-                            />
-                          </div>
-                          <div className="title-serv-c">
-                            <span>Custom</span> App Solution
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                    <div
-                      className="service-slide card-bg-c"
-                      data-tilt
-                      data-tilt-max="10"
-                      data-tilt-speed="1000"
-                    >
-                      <a href="#">
-                        <div className="service-card-hh">
-                          <div className="image-sr-mm">
-                            <img
-                              alt="custom-sport"
-                              src="./assets/images/service/startup.png"
-                            />
-                          </div>
-                          <div className="title-serv-c">
-                            <span>Startup</span> Solution
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                    <div
-                      className="service-slide card-bg-d"
-                      data-tilt
-                      data-tilt-max="10"
-                      data-tilt-speed="1000"
-                    >
-                      <a href="#">
-                        <div className="service-card-hh">
-                          <div className="image-sr-mm">
-                            <img
-                              alt="custom-sport"
-                              src="./assets/images/service/car-rental.png"
-                            />
-                          </div>
-                          <div className="title-serv-c">
-                            <span>Car</span> Rental Solution
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                    <div
-                      className="service-slide card-bg-e"
-                      data-tilt
-                      data-tilt-max="10"
-                      data-tilt-speed="1000"
-                    >
-                      <a href="#">
-                        <div className="service-card-hh">
-                          <div className="image-sr-mm">
-                            <img
-                              alt="custom-sport"
-                              src="./assets/images/service/marketing.png"
-                            />
-                          </div>
-                          <div className="title-serv-c">
-                            <span>Marketing</span> Solution
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                    <div
-                      className="service-slide card-bg-f"
-                      data-tilt
-                      data-tilt-max="10"
-                      data-tilt-speed="1000"
-                    >
-                      <a href="#">
-                        <div className="service-card-hh">
-                          <div className="image-sr-mm">
-                            <img
-                              alt="custom-sport"
-                              src="./assets/images/service/ewallet.png"
-                            />
-                          </div>
-                          <div className="title-serv-c">
-                            <span>e-Wallet</span> Solution
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            
           </div>
         </div>
       </section>
